460114afac65981fad47d428816968cdde4d9d6a
ludieziki <ludieziki@googlemail.com>
2022-08-06 19:21:04 +0300

    Файл с кодом решения задачи 1 к семинару 1

---
+++ Homework/Lesson_1_task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+Console.WriteLine("Введите проверяемое число (целое)");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число на равенство квадрату которого проверяем предыдущее");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A == B*B)
+{
+    Console.WriteLine("Да, число ");
+    Console.WriteLine(A);
+    Console.WriteLine("является квадратом ");
+    Console.WriteLine(B);    
+}
+else
+{
+    Console.WriteLine("Нет, число ");
+    Console.WriteLine(A);
+    Console.WriteLine("не является квадратом ");
+    Console.WriteLine(B);    
+}
