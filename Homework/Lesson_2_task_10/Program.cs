c4c1d43533f8a39b3486eba02129915a125752a4
ludieziki <ludieziki@googlemail.com>
2022-08-16 22:50:42 +0300

    Задача 10 к семинару 2

---
+++ Homework/Lesson_2_task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число");
+string Inp = Console.ReadLine();
+int Length = Inp.Length;
+while(Length < 3 || Length > 3)
+{
+    Console.WriteLine("Вы ввели не трехначное число, введите трехзначное число");
+    Inp = Console.ReadLine();
+    Length = Inp.Length;
+}
+int X = Convert.ToInt32(Inp);
+int H = X % 100;
+int D = (H - H % 10) / 10;
+Console.WriteLine($"Вторая цифра числа  {X}, это цифра {D}");
