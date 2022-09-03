e3b9dbcee559b8d99e54599b24d62a7d1743ec7a
ludieziki <ludieziki@googlemail.com>
2022-09-03 23:07:42 +0300

    Урок 5 задача 34

---
+++ Homework/Lesson_5_task_34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int[] array = new int[5];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 999);
+}
+void PrintArray(int[] x)
+{
+    Console.WriteLine();
+    Console.Write($"[{x[0]}");
+    for(int i = 1; i < x.Length; i++)
+    {
+        Console.Write($", {x[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+var IsEven = (int v) => v % 2 == 0;
+int CountEvenArray(int[] x)
+{
+    int n = 0;
+    for(int i = 0; i < x.Length; i++) if(IsEven(x[i]) == true) n++;
+    return n;
+}
+PrintArray(array);
+Console.WriteLine($"содержит {CountEvenArray(array)} четных элементов");
