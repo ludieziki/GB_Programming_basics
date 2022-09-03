77059fbd370420c5f89a59f58d673ae06696e495
ludieziki <ludieziki@googlemail.com>
2022-09-03 14:45:17 +0300

    Урок 4 задача 29

---
+++ Homework/Lesson_4_task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] Array = {1, 2, 5, 7, 19};
+void PrintArray(int[] x)
+{
+    Console.WriteLine();
+    Console.Write($"[{x[0]}");
+    for(int i = 1; i < x.Length; i++)
+    {
+        Console.Write($", {x[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+PrintArray(Array);
