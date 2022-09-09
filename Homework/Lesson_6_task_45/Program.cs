ba18755e7701b9c3600939de5c327cd0b135e2e4
ludieziki <ludieziki@googlemail.com>
2022-09-09 22:49:55 +0300

    Урок 6 задача 45

---
+++ Homework/Lesson_6_task_45/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int[] array = {5, 7, 8, 9, 6, 7};
+int[] reserveArray = new int[array.Length];
+void copyArray(int[] x)
+{
+    for(int i = 0; i < x.Length; i++) reserveArray[i] = x[i];
+}
+copyArray(array);
