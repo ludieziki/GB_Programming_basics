2d9f0062364610ef1351e13538e006b2ced011ec
ludieziki <ludieziki@googlemail.com>
2022-09-13 20:03:49 +0300

    Урок 7 задача 47

---
+++ Homework/Lesson_7_task_47/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+int m = 3; int n = 4;
+double[,] array = new double[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().NextDouble();
+}
