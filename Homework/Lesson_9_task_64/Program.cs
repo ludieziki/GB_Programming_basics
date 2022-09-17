62400ec19b1f74b91a1abbaa6b7e18dd8939e19b
ludieziki <ludieziki@googlemail.com>
2022-09-17 09:58:12 +0300

    Урок 9 задача 64

---
+++ Homework/Lesson_9_task_64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int n = 8;
+void PrintFromNtoOne(int x)
+{
+    if (x != 1)
+    {
+        Console.Write($"{x} ");
+        PrintFromNtoOne(x - 1);
+    }
+    else Console.Write(1);
+}
+PrintFromNtoOne(n);
